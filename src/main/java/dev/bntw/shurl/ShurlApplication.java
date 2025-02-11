2a78a26a9898b769b1eec63dc0f7794850c2c627
bloodnighttw <bbeenn1227@gmail.com>
2025-02-11 22:16:04 +0800

    feat: Init commit.

---
+++ src/main/java/dev/bntw/shurl/ShurlApplication.java
@@ -0,0 +1,13 @@
+package dev.bntw.shurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShurlApplication.class, args);
+    }
+
+}
