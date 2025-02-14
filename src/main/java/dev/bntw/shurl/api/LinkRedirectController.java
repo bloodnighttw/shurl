d2e5394c6973dc9567869a9b41134ad026a2238e
bloodnighttw <bbeenn1227@gmail.com>
2025-02-14 19:44:36 +0800

    feat: Redirect controller.

---
+++ src/main/java/dev/bntw/shurl/api/LinkRedirectController.java
@@ -0,0 +1,35 @@
+package dev.bntw.shurl.api;
+
+import dev.bntw.shurl.api.request.link.LinkRequest;
+import dev.bntw.shurl.api.response.LinkResponse;
+import dev.bntw.shurl.services.LinkService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+public class LinkRedirectController {
+
+
+    private final LinkService linkService;
+
+    @Autowired
+    public LinkRedirectController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+
+    @GetMapping("/{id}")
+    @Transactional
+    public void redirect(HttpServletResponse response, @PathVariable String id) throws IOException {
+
+        var link = linkService.getLink(id);
+        response.sendRedirect(link.getUrl());
+
+    }
+
+
+}
