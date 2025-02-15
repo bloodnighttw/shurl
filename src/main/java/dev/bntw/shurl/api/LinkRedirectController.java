28b12411b0cb1fc340dec80428b582ec54b5dab5
bloodnighttw <bbeenn1227@gmail.com>
2025-02-15 23:12:07 +0800

    feat: redirect to not found when link alias not exist.

--- src/main/java/dev/bntw/shurl/api/LinkRedirectController.java
+++ src/main/java/dev/bntw/shurl/api/LinkRedirectController.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 @RestController
 public class LinkRedirectController {
 
-
     private final LinkService linkService;
 
     @Autowired
@@ -21,15 +20,17 @@ public class LinkRedirectController {
         this.linkService = linkService;
     }
 
-
     @GetMapping("/{id}")
     @Transactional
     public void redirect(HttpServletResponse response, @PathVariable String id) throws IOException {
 
         var link = linkService.getLink(id);
+        if (link == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
         response.sendRedirect(link.getUrl());
-
     }
-
 
 }
