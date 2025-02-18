435bb953b9efdc1d2c8160e20c3ec05d4b6514f4
bloodnighttw <bbeenn1227@gmail.com>
2025-02-18 23:10:22 +0800

    feat: the auth endpoint.

---
+++ src/main/backend/dev/bntw/shurl/api/AuthController.java
@@ -0,0 +1,36 @@
+package dev.bntw.shurl.api;
+
+import dev.bntw.shurl.api.request.link.LoginRequest;
+import dev.bntw.shurl.api.response.LoginResponse;
+import dev.bntw.shurl.services.AuthService;
+import dev.bntw.shurl.services.JwtService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/${shuri.api-prefix}/auth")
+public class AuthController {
+
+    public final AuthService authService;
+    public final JwtService jwtService;
+
+    @Autowired
+    public AuthController(AuthService authService, JwtService jwtService) {
+        this.authService = authService;
+        this.jwtService = jwtService;
+    }
+
+
+    @PostMapping("/")
+    @Transactional
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        var user = authService.login(loginRequest.usernameOrEmail(), loginRequest.password());
+        var token = jwtService.createToken(user);
+        return new LoginResponse(token);
+    }
+
+}
