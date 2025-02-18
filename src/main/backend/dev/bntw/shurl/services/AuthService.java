9eed5e7956d3cf6891a9325090750ad90e28d2a3
bloodnighttw <bbeenn1227@gmail.com>
2025-02-18 23:10:07 +0800

    feat: add password check in service.

--- src/main/backend/dev/bntw/shurl/services/AuthService.java
+++ src/main/backend/dev/bntw/shurl/services/AuthService.java
@@ -1,11 +1,14 @@
 package dev.bntw.shurl.services;
 
+import dev.bntw.shurl.persistence.entity.User;
 import dev.bntw.shurl.persistence.repository.UserRepository;
 import dev.bntw.shurl.utils.MemberDetail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,10 +16,12 @@ import org.springframework.stereotype.Service;
 public class AuthService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -29,5 +34,24 @@ public class AuthService implements UserDetailsService {
         }
 
         return new MemberDetail(user);
+    }
+
+    public User login(String usernameOrEmail, String password) {
+        var user = userRepository.findByUsername(usernameOrEmail);
+        if (user == null) {
+            user = userRepository.findByEmail(usernameOrEmail);
+        }
+
+        if(user == null) {
+            throw new BadCredentialsException("Username or email not found");
+        }
+
+        var encodedPassword = user.getHashPassword();
+
+        if(passwordEncoder.matches(user.getHashPassword(), password)) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return user;
     }
 }
