93ffa4384b79bc0134008d9ad91e9227955d7c63
bloodnighttw <bbeenn1227@gmail.com>
2025-02-18 22:16:56 +0800

    chore: add jwt service.

---
+++ src/main/backend/dev/bntw/shurl/services/JwtService.java
@@ -0,0 +1,46 @@
+package dev.bntw.shurl.services;
+import dev.bntw.shurl.utils.MemberDetail;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+    private final int validSeconds;
+
+
+    public JwtService(
+            @Value("${shuri.jwt.secret}") String secretKey,
+            @Value("${shuri.jwt.valid-seconds}") int validSeconds) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.validSeconds = validSeconds;
+    }
+
+    public String createToken(MemberDetail userDetails) {
+
+        long expireMillis = Instant.now()
+                .plusSeconds(validSeconds)
+                .getEpochSecond() * 1000;
+
+        var claims = Jwts.claims()
+                .issuedAt(new Date())
+                .expiration(new Date(expireMillis))
+                .add("username", userDetails.getUsername())
+                .add("email", userDetails.getEmail())
+                .add("authorities", userDetails.getAuthorities())
+                .build();
+
+        return Jwts.builder()
+                .claims(claims)
+                .signWith(secretKey)
+                .compact();
+    }
+
+}
