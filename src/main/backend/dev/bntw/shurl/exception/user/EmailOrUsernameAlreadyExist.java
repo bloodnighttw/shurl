642c3860a82250633216e3bffc45dc7132438916
bloodnighttw <bbeenn1227@gmail.com>
2025-02-19 21:52:47 +0800

    fix: exception return code setup.

--- src/main/backend/dev/bntw/shurl/exception/user/EmailOrUsernameAlreadyExist.java
+++ src/main/backend/dev/bntw/shurl/exception/user/EmailOrUsernameAlreadyExist.java
@@ -1,6 +1,11 @@
 package dev.bntw.shurl.exception.user;
 
-public class EmailOrUsernameAlreadyExist extends Throwable {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailOrUsernameAlreadyExist extends RuntimeException {
     public EmailOrUsernameAlreadyExist(String email,String username) {
         super("Email with " + email + " exists or Username with " + username + " exists");
     }
